11fb9e9cfe8deca0fa441faa80a600cb4ad75db5
Dominik Deutsch <dominik.deutsch@gmail.com>
2019-11-28 09:03:06 +0100

    check length before output from array

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
 # public ip
 output "public_ip_address" {
-  value = "${azurerm_public_ip.oe[0].ip_address}"
+  value = length(azurerm_public_ip.oe) > 0 ? azurerm_public_ip.oe[0].ip_address : ""
 }
 # private ip
 output "private_ip_address" {
-  value = "${azurerm_network_interface.oe[0].ip_configuration[0].private_ip_address}"
+  value = length(azurerm_network_interface.oe) > 0 ? azurerm_network_interface.oe[0].ip_configuration[0].private_ip_address : ""
 }
